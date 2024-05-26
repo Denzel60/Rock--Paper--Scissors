1eaeb87c092e5a61b19bde7512bbdebd1e8014c8
Denzel <lawrencedenzel60@gmail.com>
2024-05-26 11:28:25 +0300

    Complete and responsive interface with main.js that has functionality on the rock, paper and scissors

--- main.js
+++ main.js
@@ -1,1 +1,72 @@
+let rockEl = document.getElementById("rock");
+let paperEl = document.getElementById("paper");
+let scissorsEl = document.getElementById("scissors");
+let compEl = document.getElementById("comp");
+let playerEl = document.getElementById("player");
+let resultsEl = document.getElementById("results");
+let compScoreEl = document.getElementById("compScore");
+let playerScoreEl = document.getElementById("playerScore")
 
+rockEl.addEventListener("click", ()=>{
+    let computerChoice = Math.floor((Math.random() * 3) + 1);
+    if(computerChoice === 1){
+        compEl.textContent = `✊`;
+    } else if (computerChoice === 2){
+        compEl.textContent = `✋`;
+    } else {
+        compEl.textContent = `✌️`;
+    }
+    playerEl.textContent = `✊`; 
+
+    if(computerChoice === 1 && playerEl.textContent === `✊`){
+        resultsEl.textContent = "Draw"
+    } else if (computerChoice === 2){
+        resultsEl.textContent = "Computer wins";
+    } else {
+        resultsEl.textContent = "Player Wins";
+    }
+})
+
+paperEl.addEventListener("click", ()=>{
+    let computerChoice = Math.floor((Math.random() * 3) + 1);
+    if(computerChoice === 2){
+        compEl.textContent = `✋`;
+    } else if (computerChoice === 1){
+        compEl.textContent = `✊`;
+    } else {
+        compEl.textContent = `✌️`;
+    }
+    playerEl.textContent = `✋`;
+
+    if(computerChoice === 2 && playerEl.textContent === `✋`){
+        resultsEl.textContent = "Draw"
+    } else if (computerChoice === 1){
+        resultsEl.textContent = "Player wins";
+    } else {
+        resultsEl.textContent = "Computer Wins";
+    }
+})
+
+scissorsEl.addEventListener("click", ()=>{
+    let computerChoice = Math.floor((Math.random() * 3) + 1);
+    if(computerChoice === 3){
+        compEl.textContent = `✌️`;
+    } else if (computerChoice === 1){
+        compEl.textContent = `✊`;
+    } else {
+        compEl.textContent = `✋`;
+    }
+    playerEl.textContent = `✌️`;
+
+    if(computerChoice === 3 && playerEl.textContent === `✌️`){
+        resultsEl.textContent = "Draw";
+    } else if (computerChoice === 1){
+        resultsEl.textContent = "Computer wins";
+    } else {
+        resultsEl.textContent = "Player Wins";
+    }
+});
+
+if(resultsEl.textContent = "Draw"){
+
+}
